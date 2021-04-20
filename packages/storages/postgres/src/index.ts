1b88f08495cdea4a48f7df9bfaf7a9cddb8c451b
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-04-20 22:14:04 +0300

    Test postgres connection with slonik

--- packages/storages/postgres/src/index.ts
+++ packages/storages/postgres/src/index.ts
@@ -1,7 +1,32 @@
 import { StorageProvider } from '@iqprotocol/abstract-storage';
+import { createPool, DatabasePoolType, sql } from 'slonik';
+
+export type PostgresStorageConfig = {
+  connectionUri: string;
+  accountTableName: string;
+  pool?: Parameters<typeof createPool>[1];
+};
 
 export class PostgresStorage extends StorageProvider {
-  init(): boolean {
-    return true;
+  private readonly pool: DatabasePoolType;
+  private readonly config: PostgresStorageConfig;
+
+  constructor(config: PostgresStorageConfig) {
+    super();
+    this.config = config;
+    this.pool = createPool(this.config.connectionUri, this.config.pool);
+  }
+
+  init(): void {
+    void this.pool.connect(async connection => {
+      await connection.query(
+        sql`CREATE TABLE IF NOT EXISTS ${sql.identifier([
+          this.config.accountTableName,
+        ])} (
+          address varchar(45) NOT NULL,
+          PRIMARY KEY (address)
+        );`,
+      );
+    });
   }
 }
