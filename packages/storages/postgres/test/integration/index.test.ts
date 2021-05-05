b3b52a9796610f958126100efcc81acb1f779c14
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-05-05 17:36:03 +0300

    Add postgres storage integration testing

--- packages/storages/postgres/test/integration/index.test.ts
+++ packages/storages/postgres/test/integration/index.test.ts
@@ -1,17 +1,34 @@
-/**
- * @group integration
- */
-import { PostgresStorage, PostgresStorageConfig } from '../../src';
-import { createPool, DatabasePoolType, sql } from 'slonik';
 import * as path from 'path';
+import { Account, AccountState } from '@iqprotocol/abstract-storage';
+import { PostgresStorageConfig, PostgresStore } from '../../src';
+import { createPool, DatabasePoolType, sql } from 'slonik';
+import { ensureDatabase, truncateTables } from './support/utils';
 import {
   DockerComposeEnvironment,
   StartedDockerComposeEnvironment,
 } from 'testcontainers';
 
+/**
+ * @group integration
+ */
 describe('@iqprotocol/postgres', () => {
   const CONNECTION_URI = 'postgresql://postgres@localhost:5432';
   const TEST_DATABASE_NAME = 'iqprotocol-testing';
+
+  const account: Account = {
+    id: 'test-id',
+    data: {
+      proof: 'test-proof',
+    },
+  };
+
+  const accountState: AccountState = {
+    accountId: account.id,
+    serviceId: 'test-service',
+    balance: 10n,
+    energy: 5n,
+    energyChangedAt: Math.floor(Date.now() / 1000),
+  };
 
   const config: PostgresStorageConfig = {
     connectionUri: `${CONNECTION_URI}/${TEST_DATABASE_NAME}`,
@@ -22,6 +39,9 @@ describe('@iqprotocol/postgres', () => {
   // see: https://github.com/testcontainers/testcontainers-node#docker-compose
   let environment: StartedDockerComposeEnvironment;
 
+  // isolated connection pool for utility operations & asserting
+  let pool: DatabasePoolType;
+
   beforeAll(async () => {
     environment = await new DockerComposeEnvironment(
       path.resolve(__dirname, 'support'),
@@ -30,49 +50,27 @@ describe('@iqprotocol/postgres', () => {
       .withEnv('POSTGRES_DB', TEST_DATABASE_NAME)
       .up();
 
-    const pool0 = createPool(CONNECTION_URI, {
+    await ensureDatabase(CONNECTION_URI, TEST_DATABASE_NAME);
+
+    pool = createPool(config.connectionUri, {
       maximumPoolSize: 1,
     });
-
-    await pool0.connect(async connection => {
-      await connection.query(sql`
-      SELECT pg_terminate_backend(pid)
-      FROM pg_stat_activity
-      WHERE
-        pid != pg_backend_pid() AND
-        datname = ${TEST_DATABASE_NAME}
-    `);
-      await connection.query(
-        sql`DROP DATABASE IF EXISTS ${sql.identifier([TEST_DATABASE_NAME])}`,
-      );
-      await connection.query(
-        sql`CREATE DATABASE ${sql.identifier([TEST_DATABASE_NAME])}`,
-      );
-    });
-    await pool0.end();
   });
 
   afterAll(async () => {
+    await pool.end();
     await environment.down();
   });
 
-  describe('PostgresStorage', () => {
-    // isolated connection pool for utility operations & asserting
-    let pool: DatabasePoolType;
+  describe('PostgresStore', () => {
+    let store: PostgresStore;
 
-    beforeAll(() => {
-      pool = createPool(config.connectionUri, {
-        maximumPoolSize: 1,
-      });
-    });
-
-    afterAll(async () => {
-      await pool.end();
+    beforeEach(() => {
+      store = new PostgresStore(config);
     });
 
     it('creates required tables', async () => {
-      const storage = new PostgresStorage(config);
-      await storage.init();
+      await store.init();
 
       await pool.connect(async connection => {
         const count = await connection.oneFirst(
@@ -82,6 +80,91 @@ describe('@iqprotocol/postgres', () => {
           )});`,
         );
         expect(count).toBe(2);
+      });
+    });
+
+    describe('When the store is empty', () => {
+      beforeEach(async () =>
+        truncateTables(pool, [config.accountTable, config.stateTable]),
+      );
+
+      it('does not return account data', async () => {
+        await expect(store.getAccount(account.id)).resolves.toBeNull();
+      });
+
+      it('does not return account state', async () => {
+        await expect(
+          store.getAccountState(accountState.accountId, accountState.serviceId),
+        ).resolves.toBeNull();
+      });
+
+      it('saves account', async () => {
+        await store.saveAccount(account);
+        await expect(store.getAccount(account.id)).resolves.toEqual(account);
+      });
+
+      it('does not allow to save non-existent account state', async () => {
+        await expect(store.saveAccountState(accountState)).rejects.toThrow();
+      });
+    });
+
+    describe('When account is stored', () => {
+      beforeEach(async () => store.saveAccount(account));
+
+      it('returns account data', async () => {
+        await expect(store.getAccount(account.id)).resolves.toEqual(account);
+      });
+
+      it('saves account data with the same values', async () => {
+        await store.saveAccount(account);
+        await expect(store.getAccount(account.id)).resolves.toEqual(account);
+      });
+
+      it('updates account data', async () => {
+        const updatedAccount = {
+          ...account,
+          data: {
+            proof: 'updated-proof',
+            newKey: { list: ['a', 'b'], number: 42 },
+          },
+        };
+        await store.saveAccount(updatedAccount);
+        await expect(store.getAccount(account.id)).resolves.toEqual(
+          updatedAccount,
+        );
+      });
+
+      it('saves account state', async () => {
+        await store.saveAccountState(accountState);
+        await expect(
+          store.getAccountState(accountState.accountId, accountState.serviceId),
+        ).resolves.toEqual(accountState);
+      });
+    });
+
+    describe('When account state is stored', () => {
+      beforeEach(async () => {
+        await store.saveAccount(account);
+        await store.saveAccountState(accountState);
+      });
+
+      it('returns account state', async () => {
+        await expect(
+          store.getAccountState(accountState.accountId, accountState.serviceId),
+        ).resolves.toEqual(accountState);
+      });
+
+      it('updates account state', async () => {
+        const updatedState = {
+          ...accountState,
+          balance: 15n,
+          energy: 2n,
+          energyChangedAt: Number(Date.now() / 1000),
+        };
+        await store.saveAccountState(updatedState);
+        await expect(
+          store.getAccountState(accountState.accountId, accountState.serviceId),
+        ).resolves.toEqual(updatedState);
       });
     });
   });
---
+++ packages/storages/postgres/test/integration/support/utils.ts
@@ -0,0 +1,39 @@
+import { createPool, DatabasePoolType, sql } from 'slonik';
+
+export const truncateTables = async (
+  pool: DatabasePoolType,
+  tables: string[],
+): Promise<void> => {
+  await pool.transaction(async connection => {
+    for (const tableName of tables) {
+      await connection.query(
+        sql`TRUNCATE TABLE ${sql.identifier([tableName])} CASCADE;`,
+      );
+    }
+  });
+};
+
+export const ensureDatabase = async (
+  connectionUri: string,
+  databaseName: string,
+): Promise<void> => {
+  const pool = createPool(connectionUri, {
+    maximumPoolSize: 1,
+  });
+  await pool.connect(async connection => {
+    await connection.query(sql`
+      SELECT pg_terminate_backend(pid)
+      FROM pg_stat_activity
+      WHERE
+        pid != pg_backend_pid() AND
+        datname = ${databaseName}
+    `);
+    await connection.query(
+      sql`DROP DATABASE IF EXISTS ${sql.identifier([databaseName])}`,
+    );
+    await connection.query(
+      sql`CREATE DATABASE ${sql.identifier([databaseName])}`,
+    );
+  });
+  await pool.end();
+};
