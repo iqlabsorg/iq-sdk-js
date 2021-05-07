917ace684575a87076c75e1fb3afc256b426cf89
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-05-07 19:09:12 +0300

    Adjust in-memory store config & test validator injection

--- packages/storages/in-memory/test/in-memory-store.test.ts
+++ packages/storages/in-memory/test/in-memory-store.test.ts
@@ -30,6 +30,7 @@ describe('InMemoryStore', () => {
     it('does not return account data', async () => {
       await expect(store.getAccount(account.id)).resolves.toBeNull();
     });
+
     it('does not return account state', async () => {
       await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toBeNull();
     });
@@ -46,7 +47,7 @@ describe('InMemoryStore', () => {
 
   describe('When account is stored', () => {
     beforeEach(() => {
-      store = new InMemoryStore([account]);
+      store = new InMemoryStore({ accounts: [account] });
     });
 
     it('returns account data', async () => {
@@ -70,25 +71,32 @@ describe('InMemoryStore', () => {
       await expect(store.getAccount(account.id)).resolves.toEqual(updatedAccount);
     });
 
+    it('validates account state before insert', async () => {
+      await expect(store.saveAccountState({ ...accountState, balance: -5n })).rejects.toThrow();
+      await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toBeNull();
+    });
+
     it('saves account state', async () => {
       await store.saveAccountState(accountState);
       await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toEqual(
         accountState,
       );
     });
-
-    it('validates account state before insert', async () => {
-      await expect(store.saveAccountState({ ...accountState, balance: -5n })).rejects.toThrow('Negative balance');
-      await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toBeNull();
-    });
   });
 
   describe('When account state is stored', () => {
     beforeEach(() => {
-      store = new InMemoryStore([account], [accountState]);
+      store = new InMemoryStore({ accounts: [account], states: [accountState] });
     });
 
     it('returns account state', async () => {
+      await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toEqual(
+        accountState,
+      );
+    });
+
+    it('validates account state before update', async () => {
+      await expect(store.saveAccountState({ ...accountState, balance: -5n })).rejects.toThrow();
       await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toEqual(
         accountState,
       );
@@ -106,12 +114,19 @@ describe('InMemoryStore', () => {
         updatedState,
       );
     });
+  });
 
-    it('validates account state before update', async () => {
-      await expect(store.saveAccountState({ ...accountState, balance: -5n })).rejects.toThrow('Negative balance');
-      await expect(store.getAccountState(accountState.accountId, accountState.serviceId)).resolves.toEqual(
-        accountState,
-      );
+  describe('When custom validator is provided', () => {
+    const validator = { validateAccountState: () => jest.fn() };
+    beforeEach(async () => {
+      store = new InMemoryStore({ validator });
+      await store.saveAccount(account);
+    });
+
+    it('uses provided validator to validate state', async () => {
+      const spy = jest.spyOn(validator, 'validateAccountState');
+      await store.saveAccountState(accountState);
+      expect(spy).toHaveBeenCalledWith(accountState);
     });
   });
 });
