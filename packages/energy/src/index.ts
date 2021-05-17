6e339188512f78e5dacaa2111a80206b56bcfffa
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-05-17 16:18:19 +0300

    Test energy calculation

--- packages/energy/src/index.ts
+++ packages/energy/src/index.ts
@@ -1,19 +1,31 @@
 export class Energy {
-  static halfLife(t0: number, c0: number, t12: number, t: number): number {
-    if (t < t0) {
+  static halfLife(params: { initialValue: number; halfLifePeriod: number; t0: number; t1: number }): number {
+    const { initialValue, halfLifePeriod, t0, t1 } = params;
+    const period = t1 - t0;
+
+    if (period < 0) {
       throw new Error('Invalid period');
     }
-    if (t === t0) {
-      return c0;
+    if (period === 0) {
+      return initialValue;
     }
 
-    return c0 * 0.5 ** ((t - t0) / t12);
+    return initialValue * 0.5 ** (period / halfLifePeriod);
   }
 
-  static calculateEnergy(balance: number, prevEnergy: number, t0: number, t1: number): number {
-    if (balance > prevEnergy) {
-      return balance - this.halfLife(t0, balance - prevEnergy, 100, t1);
+  static calculateEnergy(params: {
+    balance: number;
+    prevEnergy: number;
+    halfLifePeriod: number;
+    t0: number;
+    t1: number;
+  }): number {
+    const { balance, prevEnergy, halfLifePeriod, t0, t1 } = params;
+
+    if (balance < 0) {
+      throw new Error('Invalid balance');
     }
-    return balance + this.halfLife(t0, prevEnergy - balance, 100, t1);
+
+    return balance + this.halfLife({ initialValue: prevEnergy - balance, halfLifePeriod, t0, t1 });
   }
 }
--- packages/energy/test/index.test.ts
+++ packages/energy/test/index.test.ts
@@ -1,25 +1,75 @@
 import { Energy } from '../src';
 
-describe('@iqprotocol/energy', () => {
+/**
+ * @group unit
+ */
+describe('Energy', () => {
   describe('halfLife', () => {
     // Algo: c0 * 0.5 ** ((t - t0) / t12)
     test.each`
-      t0     | c0                               | t12           | t             | expected
-      ${100} | ${1000}                          | ${20}         | ${100}        | ${1000}
-      ${100} | ${1000}                          | ${20}         | ${120}        | ${500}
-      ${100} | ${1000}                          | ${20}         | ${140}        | ${250}
-      ${100} | ${1000}                          | ${20}         | ${110}        | ${707.1067811865476}
-      ${100} | ${1997.25}                       | ${20}         | ${110}        | ${1412.2690187248322}
-      ${0}   | ${1997.25}                       | ${2373046875} | ${2373046874} | ${998.6250002916901}
-      ${0}   | ${199700000001.25}               | ${2373046875} | ${2373046874} | ${99850000029.79036}
-      ${0}   | ${4503599627370449.333}          | ${2373046875} | ${2373046874} | ${2251799814342956.5}
-      ${0}   | ${Number.MAX_SAFE_INTEGER / 2.0} | ${2373046875} | ${2373046874} | ${2251799814342979.5}
-    `('returns $expected with t0 = $t0, c0 = $c0, t12 = $t12, t = $t', ({ t0, c0, t12, t, expected }) => {
-      expect(Energy.halfLife(t0, c0, t12, t)).toEqual(expected);
-    });
+      initialValue                     | halfLifePeriod | t0     | t1            | expected
+      ${100}                           | ${50}          | ${0}   | ${25}         | ${70.71067811865476}
+      ${1000}                          | ${20}          | ${100} | ${100}        | ${1000}
+      ${1000}                          | ${20}          | ${100} | ${120}        | ${500}
+      ${1000}                          | ${20}          | ${100} | ${140}        | ${250}
+      ${1000}                          | ${20}          | ${100} | ${110}        | ${707.1067811865476}
+      ${1997.25}                       | ${20}          | ${100} | ${110}        | ${1412.2690187248322}
+      ${1997.25}                       | ${2373046875}  | ${0}   | ${2373046874} | ${998.6250002916901}
+      ${199700000001.25}               | ${2373046875}  | ${0}   | ${2373046874} | ${99850000029.79036}
+      ${4503599627370449.333}          | ${2373046875}  | ${0}   | ${2373046874} | ${2251799814342956.5}
+      ${Number.MAX_SAFE_INTEGER / 2.0} | ${2373046875}  | ${0}   | ${2373046874} | ${2251799814342979.5}
+    `(
+      'returns $expected with initialValue = $initialValue, halfLifePeriod = $halfLifePeriod, t0 = $t0, t1 = $t1',
+      ({ initialValue, halfLifePeriod, t0, t1, expected }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        expect(Energy.halfLife({ initialValue, halfLifePeriod, t0, t1 })).toEqual(expected);
+      },
+    );
 
     it('throws when period is negative', () => {
-      expect(() => Energy.halfLife(100, 1000, 20, 50)).toThrow();
+      expect(() =>
+        Energy.halfLife({
+          initialValue: 1000,
+          halfLifePeriod: 50,
+          t0: 50,
+          t1: 20,
+        }),
+      ).toThrowError('Invalid period');
+    });
+  });
+
+  describe('calculateEnergy', () => {
+    test.each`
+      balance | prevEnergy | halfLifePeriod             | t0   | t1     | expected
+      ${100}  | ${0}       | ${50}                      | ${0} | ${0}   | ${0}
+      ${100}  | ${0}       | ${50}                      | ${0} | ${25}  | ${29.289321881345245}
+      ${100}  | ${0}       | ${50}                      | ${0} | ${50}  | ${50}
+      ${100}  | ${10}      | ${50}                      | ${0} | ${50}  | ${55}
+      ${100}  | ${0}       | ${50}                      | ${0} | ${100} | ${75}
+      ${100}  | ${0}       | ${50}                      | ${0} | ${150} | ${87.5}
+      ${100}  | ${0}       | ${50}                      | ${0} | ${200} | ${93.75}
+      ${100}  | ${200}     | ${50}                      | ${0} | ${25}  | ${170.71067811865476}
+      ${100}  | ${200}     | ${50}                      | ${0} | ${50}  | ${150}
+      ${100}  | ${200}     | ${50}                      | ${0} | ${100} | ${125}
+      ${1}    | ${0}       | ${Number.MAX_SAFE_INTEGER} | ${0} | ${1}   | ${1.1102230246251565e-16}
+    `(
+      'returns $expected with balance = $balance, prevEnergy = $prevEnergy, halfLifePeriod = $halfLifePeriod, t0 = $t0, t1 = $t1',
+      ({ balance, prevEnergy, halfLifePeriod, t0, t1, expected }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        expect(Energy.calculateEnergy({ balance, prevEnergy, halfLifePeriod, t0, t1 })).toEqual(expected);
+      },
+    );
+
+    it('throws when the balance is negative', () => {
+      expect(() =>
+        Energy.calculateEnergy({
+          balance: -1,
+          prevEnergy: 0,
+          halfLifePeriod: 50,
+          t0: 50,
+          t1: 20,
+        }),
+      ).toThrowError('Invalid balance');
     });
   });
 });
