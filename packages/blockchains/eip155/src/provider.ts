0c1626fcbf01dd54b41b8bdcdf30a34b9ab87738
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-06-16 13:23:42 +0300

    Update EIP155 provider
    - get ERC20 & ERC721 tokens metadata
    - allow to set liquidity token allowance for enterprise

--- packages/blockchains/eip155/src/provider.ts
+++ packages/blockchains/eip155/src/provider.ts
@@ -1,5 +1,16 @@
-import { BigNumberish, ContractTransaction, Signer } from 'ethers';
-import { Enterprise__factory, EnterpriseFactory__factory, PowerToken__factory } from '../types/contracts';
+import { BigNumber, BigNumberish, ContractTransaction, Signer } from 'ethers';
+import {
+  Enterprise,
+  Enterprise__factory,
+  EnterpriseFactory,
+  EnterpriseFactory__factory,
+  ERC20,
+  ERC20__factory,
+  ERC721,
+  ERC721__factory,
+  PowerToken,
+  PowerToken__factory,
+} from '../types/contracts';
 import {
   AccountState,
   Address,
@@ -10,6 +21,20 @@ import {
   ServiceInfo,
   ServiceParams,
 } from '@iqprotocol/abstract-blockchain';
+
+export interface ERC20Metadata {
+  address: Address;
+  name: string;
+  symbol: string;
+  decimals: number;
+}
+
+export interface ERC721Metadata {
+  address: Address;
+  name: string;
+  symbol: string;
+  tokenUri: string;
+}
 
 type DeployedContracts = {
   enterpriseFactory: Address;
@@ -36,7 +61,7 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async deployEnterprise(params: EnterpriseParams): Promise<ContractTransaction> {
-    return EnterpriseFactory__factory.connect(this.contracts.enterpriseFactory, this.signer).deploy(
+    return this.resolveEnterpriseFactory().deploy(
       params.name,
       params.liquidityTokenAddress,
       params.baseUri,
@@ -46,12 +71,11 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async getEnterpriseServices(enterpriseAddress: Address): Promise<Address[]> {
-    const enterprise = Enterprise__factory.connect(enterpriseAddress, this.signer);
-    return enterprise.getPowerTokens();
+    return this.resolveEnterprise(enterpriseAddress).getPowerTokens();
   }
 
   async getEnterpriseInfo(enterpriseAddress: Address): Promise<EnterpriseInfo> {
-    const enterprise = Enterprise__factory.connect(enterpriseAddress, this.signer);
+    const enterprise = this.resolveEnterprise(enterpriseAddress);
     const {
       name,
       baseUri,
@@ -85,7 +109,7 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async registerService(enterpriseAddress: Address, params: ServiceParams): Promise<ContractTransaction> {
-    const enterprise = Enterprise__factory.connect(enterpriseAddress, this.signer);
+    const enterprise = this.resolveEnterprise(enterpriseAddress);
     return enterprise.registerService(
       params.name,
       params.symbol,
@@ -101,7 +125,7 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async getServiceInfo(serviceAddress: Address): Promise<ServiceInfo> {
-    const powerToken = PowerToken__factory.connect(serviceAddress, this.signer);
+    const powerToken = this.resolvePowerToken(serviceAddress);
 
     const {
       name,
@@ -134,7 +158,7 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async getAccountState(serviceAddress: Address, accountAddress: Address): Promise<AccountState> {
-    const powerToken = PowerToken__factory.connect(serviceAddress, this.signer);
+    const powerToken = this.resolvePowerToken(serviceAddress);
     const balance = await powerToken.balanceOf(accountAddress);
     const { energy, timestamp } = await powerToken.getState(accountAddress);
     return {
@@ -147,7 +171,65 @@ export class EIP155BlockchainProvider implements BlockchainProvider<ContractTran
   }
 
   async addLiquidity(enterpriseAddress: Address, amount: BigNumberish): Promise<ContractTransaction> {
-    const enterprise = Enterprise__factory.connect(enterpriseAddress, this.signer);
+    const enterprise = this.resolveEnterprise(enterpriseAddress);
     return enterprise.addLiquidity(amount);
+  }
+
+  async approveLiquidityToken(enterpriseAddress: Address, amount: BigNumberish): Promise<ContractTransaction> {
+    const enterprise = this.resolveEnterprise(enterpriseAddress);
+    const liquidityTokenAddress = await enterprise.getLiquidityToken();
+    const liquidityToken = this.resolveERC20Token(liquidityTokenAddress);
+    return liquidityToken.approve(enterprise.address, amount);
+  }
+
+  async getLiquidityTokenAllowance(enterpriseAddress: Address): Promise<BigNumber> {
+    const enterprise = this.resolveEnterprise(enterpriseAddress);
+    const liquidityTokenAddress = await enterprise.getLiquidityToken();
+    const liquidityToken = this.resolveERC20Token(liquidityTokenAddress);
+    const signerAddress = await this.signer.getAddress();
+    return liquidityToken.allowance(signerAddress, enterprise.address);
+  }
+
+  async getLiquidityTokenAddress(enterpriseAddress: Address): Promise<Address> {
+    return this.resolveEnterprise(enterpriseAddress).getLiquidityToken();
+  }
+
+  async getLiquidityTokenMetadata(enterpriseAddress: Address): Promise<ERC20Metadata> {
+    const tokenAddress = await this.resolveEnterprise(enterpriseAddress).getLiquidityToken();
+    return this.getERC20Metadata(tokenAddress);
+  }
+
+  async getERC20Metadata(address: Address): Promise<ERC20Metadata> {
+    const token = this.resolveERC20Token(address);
+    const [name, symbol, decimals] = await Promise.all([token.name(), token.symbol(), token.decimals()]);
+
+    return { address, name, symbol, decimals };
+  }
+
+  async getERC721Metadata(address: Address, tokenId: BigNumberish): Promise<ERC721Metadata> {
+    const token = this.resolveERC721Token(address);
+    const [name, symbol, tokenUri] = await Promise.all([token.name(), token.symbol(), token.tokenURI(tokenId)]);
+
+    return { address, name, symbol, tokenUri };
+  }
+
+  protected resolveEnterpriseFactory(): EnterpriseFactory {
+    return EnterpriseFactory__factory.connect(this.contracts.enterpriseFactory, this.signer);
+  }
+
+  protected resolveEnterprise(enterpriseAddress: string): Enterprise {
+    return Enterprise__factory.connect(enterpriseAddress, this.signer);
+  }
+
+  protected resolvePowerToken(serviceAddress: string): PowerToken {
+    return PowerToken__factory.connect(serviceAddress, this.signer);
+  }
+
+  protected resolveERC20Token(tokenAddress: string): ERC20 {
+    return ERC20__factory.connect(tokenAddress, this.signer);
+  }
+
+  protected resolveERC721Token(tokenAddress: string): ERC721 {
+    return ERC721__factory.connect(tokenAddress, this.signer);
   }
 }
