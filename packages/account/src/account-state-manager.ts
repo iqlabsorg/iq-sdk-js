23de6cace63930a362a83e4b3d11251fc5352fd8
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-06-29 12:59:02 +0300

    Introduce account package

---
+++ packages/account/src/account-state-manager.ts
@@ -0,0 +1,159 @@
+import { AccountState, AccountStateChangeResult, StorageProvider } from '@iqprotocol/abstract-storage';
+import { AccountID, BlockchainProvider, AccountState as OnChainAccountState } from '@iqprotocol/abstract-blockchain';
+
+export type OnChainAccountStateReader = Pick<BlockchainProvider, 'getAccountState'>;
+
+export interface AccountStateManagerConfig {
+  blockchain: OnChainAccountStateReader;
+  store: StorageProvider;
+}
+
+export class AccountStateManager {
+  private readonly blockchain: OnChainAccountStateReader;
+  private readonly store: StorageProvider;
+
+  constructor({ blockchain, store }: AccountStateManagerConfig) {
+    this.blockchain = blockchain;
+    this.store = store;
+  }
+
+  async initAccountState({
+    serviceId,
+    accountId,
+    power,
+    lockedPower,
+    energy,
+    energyCalculatedAt,
+  }: Omit<AccountState, 'serviceId' | 'accountId'> & {
+    serviceId: AccountID;
+    accountId: AccountID;
+  }): Promise<AccountState> {
+    this.validateSameChain(serviceId, accountId);
+    return this.store.initAccountState({
+      serviceId: serviceId.toString(),
+      accountId: accountId.toString(),
+      power,
+      lockedPower,
+      energy,
+      energyCalculatedAt,
+    });
+  }
+
+  async initAccountStateFromBlockchain(serviceId: AccountID, accountId: AccountID): Promise<AccountState> {
+    this.validateSameChain(serviceId, accountId);
+    try {
+      const { balance, energy, timestamp } = await this.getBlockchainAccountState(serviceId, accountId);
+      return await this.initAccountState({
+        serviceId,
+        accountId,
+        power: balance.toBigInt(),
+        lockedPower: 0n,
+        energy: energy.toBigInt(),
+        energyCalculatedAt: timestamp,
+      });
+    } catch (e) {
+      throw new Error(`Failed to initialize account state. ${e}`);
+    }
+  }
+
+  async getBlockchainAccountState(serviceId: AccountID, accountId: AccountID): Promise<OnChainAccountState> {
+    return this.blockchain.getAccountState(serviceId.address, accountId.address);
+  }
+
+  async getAccountState(serviceId: AccountID, accountId: AccountID): Promise<AccountState | null> {
+    return this.store.getAccountState(serviceId.toString(), accountId.toString());
+  }
+
+  async getInitializedAccountState(serviceId: AccountID, accountId: AccountID): Promise<AccountState> {
+    this.validateSameChain(serviceId, accountId);
+    const currentState = await this.getAccountState(serviceId, accountId);
+    return currentState ? currentState : this.initAccountStateFromBlockchain(serviceId, accountId);
+  }
+
+  async increasePower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    power: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    return this.changePower(serviceId, accountId, power, timestamp);
+  }
+
+  async decreasePower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    power: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    return this.changePower(serviceId, accountId, -power, timestamp);
+  }
+
+  async lockPower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    power: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    return this.changeLockedPower(serviceId, accountId, power, timestamp);
+  }
+
+  async unlockPower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    power: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    return this.changeLockedPower(serviceId, accountId, -power, timestamp);
+  }
+
+  async spendEnergy(
+    serviceId: AccountID,
+    accountId: AccountID,
+    energy: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    const currentState = await this.getInitializedAccountState(serviceId, accountId);
+    const newState = <AccountState>{
+      ...currentState,
+      energy: currentState.energy - energy,
+      energyCalculatedAt: Math.floor(timestamp.getTime() / 1000),
+    };
+    return this.store.changeAccountState(currentState, newState);
+  }
+
+  protected async changeLockedPower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    lockedPowerDelta: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    const currentState = await this.getInitializedAccountState(serviceId, accountId);
+    const newState = <AccountState>{
+      ...currentState,
+      lockedPower: currentState.lockedPower + lockedPowerDelta,
+      energyCalculatedAt: Math.floor(timestamp.getTime() / 1000),
+    };
+    return this.store.changeAccountState(currentState, newState);
+  }
+
+  protected async changePower(
+    serviceId: AccountID,
+    accountId: AccountID,
+    powerDelta: bigint,
+    timestamp: Date,
+  ): Promise<AccountStateChangeResult> {
+    const currentState = await this.getInitializedAccountState(serviceId, accountId);
+    const newState = <AccountState>{
+      ...currentState,
+      power: currentState.power + powerDelta,
+      energyCalculatedAt: Math.floor(timestamp.getTime() / 1000),
+    };
+    return this.store.changeAccountState(currentState, newState);
+  }
+
+  protected validateSameChain(id1: AccountID, id2: AccountID): void {
+    if (id1.chainId.toString() !== id2.chainId.toString()) {
+      throw new Error('Chain ID mismatch');
+    }
+  }
+}
---
+++ packages/account/test/account-state-manager.test.ts
@@ -0,0 +1,176 @@
+import { InMemoryStore } from '@iqprotocol/in-memory-storage';
+import { Account, AccountState, AccountStateChangeResult } from '@iqprotocol/abstract-storage';
+import { AccountStateManager, OnChainAccountStateReader } from '../src';
+import { AccountID, AccountState as OnChainAccountState, BigNumber, ChainID } from '@iqprotocol/abstract-blockchain';
+
+/**
+ * @group unit
+ */
+describe('AccountStateManager', () => {
+  const now = new Date();
+  const timestamp = Math.floor(now.getTime() / 1000);
+  const serviceAddress = '0x36F898881c06f636b7aFAAcA35d652Bc38261C4b';
+  const accountAddress = '0xcC0070C1E41b82c95C5a76A564F78301b1B8b3CB';
+  const onChainAccountState = <OnChainAccountState>{
+    serviceAddress,
+    accountAddress,
+    balance: BigNumber.from(10),
+    energy: BigNumber.from(5),
+    timestamp,
+  };
+
+  const onChainAccountStateReader: OnChainAccountStateReader = {
+    getAccountState: jest.fn().mockResolvedValue(onChainAccountState),
+  };
+  const chainId = new ChainID({ namespace: 'eip155', reference: '1' });
+  const serviceId = new AccountID({ chainId, address: serviceAddress });
+  const accountId = new AccountID({ chainId, address: accountAddress });
+  const account: Account = {
+    id: accountId.toString(),
+    data: {
+      proof: 'test-proof',
+    },
+  };
+  const accountState: AccountState = {
+    accountId: account.id,
+    serviceId: serviceId.toString(),
+    power: 10n,
+    lockedPower: 0n,
+    energy: 5n,
+    energyCalculatedAt: timestamp,
+  };
+
+  let store: InMemoryStore;
+  let accountStateManager: AccountStateManager;
+
+  beforeEach(() => {
+    store = new InMemoryStore();
+    accountStateManager = new AccountStateManager({
+      store,
+      blockchain: onChainAccountStateReader,
+    });
+  });
+
+  describe('When account is not registered', () => {
+    it('throws upon increasing power', async () => {
+      await expect(accountStateManager.increasePower(serviceId, accountId, 1n, now)).rejects.toThrow();
+    });
+
+    it('throws upon decreasing power', async () => {
+      await expect(accountStateManager.decreasePower(serviceId, accountId, 1n, now)).rejects.toThrow();
+    });
+
+    it('throws upon locking power', async () => {
+      await expect(accountStateManager.lockPower(serviceId, accountId, 1n, now)).rejects.toThrow();
+    });
+
+    it('throws upon unlocking power', async () => {
+      await expect(accountStateManager.unlockPower(serviceId, accountId, 1n, now)).rejects.toThrow();
+    });
+
+    it('throws upon spending energy', async () => {
+      await expect(accountStateManager.spendEnergy(serviceId, accountId, 1n, now)).rejects.toThrow();
+    });
+  });
+
+  describe('When account is registered', () => {
+    beforeEach(async () => {
+      await store.saveAccount(account);
+    });
+    describe('When account state is not initialized', () => {
+      it('returns null upon account state request', async () => {
+        await expect(accountStateManager.getAccountState(serviceId, accountId)).resolves.toBeNull();
+      });
+
+      it('returns state from blockchain', async () => {
+        await expect(accountStateManager.getBlockchainAccountState(serviceId, accountId)).resolves.toEqual(
+          onChainAccountState,
+        );
+      });
+
+      it('returns initialized state', async () => {
+        await expect(accountStateManager.getInitializedAccountState(serviceId, accountId)).resolves.toEqual(
+          accountState,
+        );
+      });
+
+      it('increases power based on blockchain data', async () => {
+        const result = await accountStateManager.increasePower(serviceId, accountId, 3n, now);
+        expect(result).toEqual(<AccountStateChangeResult>{
+          successful: true,
+          currentState: { ...accountState, power: accountState.power + 3n },
+        });
+      });
+
+      it('decreases power based on blockchain data', async () => {
+        const result = await accountStateManager.decreasePower(serviceId, accountId, 3n, now);
+        expect(result).toEqual(<AccountStateChangeResult>{
+          successful: true,
+          currentState: { ...accountState, power: accountState.power - 3n },
+        });
+      });
+
+      it('locks power based on blockchain data', async () => {
+        const result = await accountStateManager.lockPower(serviceId, accountId, 3n, now);
+        expect(result).toEqual(<AccountStateChangeResult>{
+          successful: true,
+          currentState: { ...accountState, lockedPower: accountState.lockedPower + 3n },
+        });
+      });
+
+      it('throws upon unlocking power', async () => {
+        await expect(accountStateManager.unlockPower(serviceId, accountId, 1n, now)).rejects.toThrow();
+      });
+
+      describe('When account state is initialized', () => {
+        let initialState: AccountState;
+        beforeEach(async () => {
+          initialState = { ...accountState, power: 15n, lockedPower: 7n };
+          await store.initAccountState(initialState);
+        });
+
+        it('returns correct state', async () => {
+          await expect(accountStateManager.getAccountState(serviceId, accountId)).resolves.toEqual(initialState);
+        });
+
+        it('returns initialized state', async () => {
+          await expect(accountStateManager.getInitializedAccountState(serviceId, accountId)).resolves.toEqual(
+            initialState,
+          );
+        });
+
+        it('increases power based on stored data', async () => {
+          const result = await accountStateManager.increasePower(serviceId, accountId, 3n, now);
+          expect(result).toEqual(<AccountStateChangeResult>{
+            successful: true,
+            currentState: { ...initialState, power: initialState.power + 3n },
+          });
+        });
+
+        it('decreases power based on stored data', async () => {
+          const result = await accountStateManager.decreasePower(serviceId, accountId, 3n, now);
+          expect(result).toEqual(<AccountStateChangeResult>{
+            successful: true,
+            currentState: { ...initialState, power: initialState.power - 3n },
+          });
+        });
+
+        it('locks power based on stored data', async () => {
+          const result = await accountStateManager.lockPower(serviceId, accountId, 3n, now);
+          expect(result).toEqual(<AccountStateChangeResult>{
+            successful: true,
+            currentState: { ...initialState, lockedPower: initialState.lockedPower + 3n },
+          });
+        });
+
+        it('unlocks power based on stored data', async () => {
+          const result = await accountStateManager.unlockPower(serviceId, accountId, 3n, now);
+          expect(result).toEqual(<AccountStateChangeResult>{
+            successful: true,
+            currentState: { ...initialState, lockedPower: initialState.lockedPower - 3n },
+          });
+        });
+      });
+    });
+  });
+});
