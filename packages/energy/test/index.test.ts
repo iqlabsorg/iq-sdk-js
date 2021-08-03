332b3062b46cefc6c3a11fc712b84ee4a00f594f
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-08-03 16:24:44 +0300

    Naming

--- packages/energy/test/index.test.ts
+++ packages/energy/test/index.test.ts
@@ -3,7 +3,7 @@ import { calculateEnergyCap, halfLife } from '../src';
 /**
  * @group unit
  */
-describe('Energy', () => {
+describe('Energy Utils', () => {
   describe('halfLife', () => {
     // Algo: c0 * 0.5 ** ((t - t0) / t12)
     test.each`
@@ -19,7 +19,7 @@ describe('Energy', () => {
       ${4503599627370449.333}          | ${2373046875}  | ${0}   | ${2373046874} | ${2251799814342956.5}
       ${Number.MAX_SAFE_INTEGER / 2.0} | ${2373046875}  | ${0}   | ${2373046874} | ${2251799814342979.5}
     `(
-      'returns $expected with initialValue = $initialValue, halfLifePeriod = $halfLifePeriod, t0 = $t0, t1 = $t1',
+      'returns $expected when: initialValue = $initialValue, halfLifePeriod = $halfLifePeriod, t0 = $t0, t1 = $t1',
       ({ initialValue, halfLifePeriod, t0, t1, expected }) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         expect(halfLife({ initialValue, halvingPeriod: halfLifePeriod, t0, t1 })).toEqual(expected);
@@ -40,36 +40,36 @@ describe('Energy', () => {
 
   describe('energy cap', () => {
     test.each`
-      balance | prevEnergy | halfLifePeriod             | t0   | t1     | expected
-      ${100}  | ${0}       | ${50}                      | ${0} | ${0}   | ${0}
-      ${100}  | ${0}       | ${50}                      | ${0} | ${25}  | ${29.289321881345245}
-      ${100}  | ${0}       | ${50}                      | ${0} | ${50}  | ${50}
-      ${100}  | ${10}      | ${50}                      | ${0} | ${50}  | ${55}
-      ${100}  | ${0}       | ${50}                      | ${0} | ${100} | ${75}
-      ${100}  | ${0}       | ${50}                      | ${0} | ${150} | ${87.5}
-      ${100}  | ${0}       | ${50}                      | ${0} | ${200} | ${93.75}
-      ${100}  | ${200}     | ${50}                      | ${0} | ${25}  | ${170.71067811865476}
-      ${100}  | ${200}     | ${50}                      | ${0} | ${50}  | ${150}
-      ${100}  | ${200}     | ${50}                      | ${0} | ${100} | ${125}
-      ${1}    | ${0}       | ${Number.MAX_SAFE_INTEGER} | ${0} | ${1}   | ${1.1102230246251565e-16}
+      power  | prevEnergyCap | halvingPeriod              | t0   | t1     | expected
+      ${100} | ${0}          | ${50}                      | ${0} | ${0}   | ${0}
+      ${100} | ${0}          | ${50}                      | ${0} | ${25}  | ${29.289321881345245}
+      ${100} | ${0}          | ${50}                      | ${0} | ${50}  | ${50}
+      ${100} | ${10}         | ${50}                      | ${0} | ${50}  | ${55}
+      ${100} | ${0}          | ${50}                      | ${0} | ${100} | ${75}
+      ${100} | ${0}          | ${50}                      | ${0} | ${150} | ${87.5}
+      ${100} | ${0}          | ${50}                      | ${0} | ${200} | ${93.75}
+      ${100} | ${200}        | ${50}                      | ${0} | ${25}  | ${170.71067811865476}
+      ${100} | ${200}        | ${50}                      | ${0} | ${50}  | ${150}
+      ${100} | ${200}        | ${50}                      | ${0} | ${100} | ${125}
+      ${1}   | ${0}          | ${Number.MAX_SAFE_INTEGER} | ${0} | ${1}   | ${1.1102230246251565e-16}
     `(
-      'returns $expected with balance = $balance, prevEnergy = $prevEnergy, halfLifePeriod = $halfLifePeriod, t0 = $t0, t1 = $t1',
-      ({ balance, prevEnergy, halfLifePeriod, t0, t1, expected }) => {
+      'returns $expected when: power = $power, prevEnergyCap = $prevEnergyCap, halvingPeriod = $halvingPeriod, t0 = $t0, t1 = $t1',
+      ({ power, prevEnergyCap, halvingPeriod, t0, t1, expected }) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        expect(calculateEnergyCap({ balance, prevEnergy, halvingPeriod: halfLifePeriod, t0, t1 })).toEqual(expected);
+        expect(calculateEnergyCap({ power, prevEnergyCap, halvingPeriod, t0, t1 })).toEqual(expected);
       },
     );
 
     it('throws when the balance is negative', () => {
       expect(() =>
         calculateEnergyCap({
-          balance: -1,
-          prevEnergy: 0,
+          power: -1,
+          prevEnergyCap: 0,
           halvingPeriod: 50,
           t0: 50,
           t1: 20,
         }),
-      ).toThrowError('Invalid balance');
+      ).toThrowError('Negative power');
     });
   });
 });
