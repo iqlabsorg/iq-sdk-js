f90a53c3a9d0c1c0384466f386ed7ddd1c70b09c
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-08-05 16:21:29 +0300

    Add effective energy calculations

--- packages/energy/src/index.ts
+++ packages/energy/src/index.ts
@@ -1,10 +1,25 @@
-/* eslint-disable func-style,prettier/prettier */
+/* eslint-disable no-extra-parens */
 const ONE = 1n << 144n;
 const LOG_ONE_HALF = 15457698658747239244624307340191628289589491n; // log(0.5) * 2 ** 144
-const MAX_SAFE_UINT112 = 5192296858534827628530496329220095n // 2 ** 112 - 1;
+const MAX_SAFE_UINT112 = 5192296858534827628530496329220095n; // 2 ** 112 - 1;
 
-export function halfLife(params: { initialValue: bigint; gapHalvingPeriod: number; t0: number; t1: number }): bigint {
+export type HalfLifeParams = {
+  initialValue: bigint;
+  gapHalvingPeriod: number;
+  t0: number;
+  t1: number;
+};
 
+export type EnergyCalculationParams = HalfLifeParams & {
+  power: bigint;
+};
+
+export type EffectiveEnergyCalculationParams = Omit<EnergyCalculationParams, 'initialValue'> & {
+  energy: bigint;
+  energyCap: bigint;
+};
+
+export const halfLife = (params: HalfLifeParams): bigint => {
   const { gapHalvingPeriod, t0, t1 } = params;
   let { initialValue } = params;
 
@@ -16,12 +31,12 @@ export function halfLife(params: { initialValue: bigint; gapHalvingPeriod: numbe
     throw new Error('Initial value overflow');
   }
 
-  let period = t1 - t0;
-  if (period < 0) {
+  let period = BigInt(t1 - t0);
+  if (period < 0n) {
     throw new Error('Negative period');
   }
 
-  if (period === 0) {
+  if (period === 0n) {
     return initialValue;
   }
 
@@ -30,38 +45,57 @@ export function halfLife(params: { initialValue: bigint; gapHalvingPeriod: numbe
     return initialValue;
   }
 
-  period %= gapHalvingPeriod;
-  const x = BigInt(LOG_ONE_HALF) * BigInt(period) / BigInt(gapHalvingPeriod);
+  period %= BigInt(gapHalvingPeriod);
+  const x = BigInt(LOG_ONE_HALF * period) / BigInt(gapHalvingPeriod);
   let z = BigInt(initialValue);
   let i = ONE;
   let sum = 0n;
 
   while (z !== 0n) {
     sum += z;
-    z = z * x / i;
+    z = (z * x) / i;
     i += ONE;
     sum -= z;
-    z = z * x / i;
+    z = (z * x) / i;
     i += ONE;
   }
 
   return BigInt.asUintN(112, sum);
-}
+};
 
-export function calculateEnergyCap(params: {
-  power: bigint;
-  prevEnergyCap: bigint;
-  gapHalvingPeriod: number;
-  t0: number;
-  t1: number;
-}): bigint {
-  const { power, prevEnergyCap, gapHalvingPeriod, t0, t1 } = params;
-  if (power < 0) {
-    throw new Error('Negative power');
+export const calculateLinearEnergy = ({
+  power,
+  initialValue,
+  gapHalvingPeriod,
+  t0,
+  t1,
+}: EnergyCalculationParams): bigint => {
+  const period = BigInt(t1 - t0);
+  if (period < 0n) {
+    throw new Error('Negative period');
   }
+  return initialValue + (power * period) / BigInt(gapHalvingPeriod * 4);
+};
 
-  if (power > prevEnergyCap) {
-    return power - halfLife({ initialValue: power - prevEnergyCap, gapHalvingPeriod, t0, t1 });
+export const calculateEnergyCap = ({
+  power,
+  initialValue,
+  gapHalvingPeriod,
+  t0,
+  t1,
+}: EnergyCalculationParams): bigint => {
+  if (power > initialValue) {
+    return power - halfLife({ initialValue: power - initialValue, gapHalvingPeriod, t0, t1 });
   }
-  return power + halfLife({ initialValue: prevEnergyCap - power, gapHalvingPeriod, t0, t1 });
-}
+  return power + halfLife({ initialValue: initialValue - power, gapHalvingPeriod, t0, t1 });
+};
+
+export const calculateEffectiveEnergy = (
+  params: EffectiveEnergyCalculationParams,
+): { energyCap: bigint; linearEnergy: bigint; energy: bigint } => {
+  const energyCap = calculateEnergyCap({ ...params, initialValue: params.energyCap });
+  const linearEnergy = calculateLinearEnergy({ ...params, initialValue: params.energy });
+  const energy = linearEnergy < energyCap ? linearEnergy : energyCap; // min(linearEnergy, energyCap)
+
+  return { energyCap, linearEnergy, energy };
+};
--- packages/energy/test/index.test.ts
+++ packages/energy/test/index.test.ts
@@ -1,11 +1,17 @@
 /* eslint-disable no-extra-parens */
-import { calculateEnergyCap, halfLife } from '../src';
+import {
+  calculateEffectiveEnergy,
+  calculateEnergyCap,
+  calculateLinearEnergy,
+  EnergyCalculationParams,
+  halfLife,
+} from '../src';
 
 /**
  * @group unit
  */
 describe('Energy Utils', () => {
-  describe('halfLife', () => {
+  describe('Half life', () => {
     // Algo: c0 * 0.5 ** ((t - t0) / t12)
     test.each`
       initialValue                           | gapHalvingPeriod | t0     | t1            | expected
@@ -17,9 +23,16 @@ describe('Energy Utils', () => {
       ${5192296858534774017680532110835712n} | ${2373046875}    | ${0}   | ${2373046874} | ${2596148430025700290926036596036491n}
       ${5192296858534827052069744025796608n} | ${2373046875}    | ${0}   | ${2373046874} | ${2596148430025726808120650298968105n}
     `(
-      'correct when: initialValue = $initialValue, halfLifePeriod = $gapHalvingPeriod, t0 = $t0, t1 = $t1',
-      ({ initialValue, gapHalvingPeriod, t0, t1, expected }) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      'equals $expected when: initialValue = $initialValue, halfLifePeriod = $gapHalvingPeriod, t0 = $t0, t1 = $t1',
+      ({
+        initialValue,
+        gapHalvingPeriod,
+        t0,
+        t1,
+        expected,
+      }: EnergyCalculationParams & {
+        expected: bigint;
+      }) => {
         expect(halfLife({ initialValue, gapHalvingPeriod: gapHalvingPeriod, t0, t1 })).toEqual(expected);
       },
     );
@@ -36,40 +49,80 @@ describe('Energy Utils', () => {
     });
   });
 
-  describe('energy cap', () => {
+  describe('Linear energy', () => {
     test.each`
-      power   | prevEnergyCap | gapHalvingPeriod           | t0   | t1     | expected
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${0}   | ${0n}
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${25}  | ${29n}
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${50}  | ${50n}
-      ${100n} | ${10n}        | ${50}                      | ${0} | ${50}  | ${55n}
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${100} | ${75n}
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${150} | ${88n}
-      ${100n} | ${0n}         | ${50}                      | ${0} | ${200} | ${94n}
-      ${100n} | ${200n}       | ${50}                      | ${0} | ${25}  | ${171n}
-      ${100n} | ${200n}       | ${50}                      | ${0} | ${50}  | ${150n}
-      ${100n} | ${200n}       | ${50}                      | ${0} | ${100} | ${125n}
-      ${1n}   | ${0n}         | ${Number.MAX_SAFE_INTEGER} | ${0} | ${1}   | ${0n}
+      power    | initialValue | gapHalvingPeriod           | t0   | t1                         | expected
+      ${0n}    | ${200n}      | ${50}                      | ${0} | ${50}                      | ${200n}
+      ${5n}    | ${200n}      | ${50}                      | ${0} | ${50}                      | ${201n}
+      ${100n}  | ${200n}      | ${50}                      | ${0} | ${25}                      | ${212n}
+      ${100n}  | ${200n}      | ${50}                      | ${0} | ${50}                      | ${225n}
+      ${100n}  | ${200n}      | ${50}                      | ${0} | ${100}                     | ${250n}
+      ${-100n} | ${200n}      | ${50}                      | ${0} | ${50}                      | ${175n}
+      ${-100n} | ${200n}      | ${50}                      | ${0} | ${25}                      | ${188n}
+      ${-100n} | ${200n}      | ${50}                      | ${0} | ${100}                     | ${150n}
+      ${-100n} | ${-200n}     | ${50}                      | ${0} | ${100}                     | ${-250n}
+      ${-200n} | ${-100n}     | ${50}                      | ${0} | ${100}                     | ${-200n}
+      ${300n}  | ${0n}        | ${Number.MAX_SAFE_INTEGER} | ${0} | ${Number.MAX_SAFE_INTEGER} | ${75n}
     `(
-      'correct when: power = $power, prevEnergyCap = $prevEnergyCap, gapHalvingPeriod = $gapHalvingPeriod, t0 = $t0, t1 = $t1',
-      ({ power, prevEnergyCap, gapHalvingPeriod, t0, t1, expected }) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        expect(calculateEnergyCap({ power, prevEnergyCap, gapHalvingPeriod: gapHalvingPeriod, t0, t1 })).toEqual(
-          expected,
-        );
+      'equals $expected when: power = $power, initialValue = $initialValue, gapHalvingPeriod = $gapHalvingPeriod, t0 = $t0, t1 = $t1',
+      ({
+        power,
+        initialValue,
+        gapHalvingPeriod,
+        t0,
+        t1,
+        expected,
+      }: EnergyCalculationParams & {
+        expected: bigint;
+      }) => {
+        expect(calculateLinearEnergy({ power, initialValue, gapHalvingPeriod, t0, t1 })).toEqual(expected);
       },
     );
+  });
 
-    it('throws when the balance is negative', () => {
-      expect(() =>
-        calculateEnergyCap({
-          power: -1n,
-          prevEnergyCap: 0n,
+  describe('Energy cap', () => {
+    test.each`
+      power   | initialValue | gapHalvingPeriod           | t0   | t1     | expected
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${0}   | ${0n}
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${25}  | ${29n}
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${50}  | ${50n}
+      ${100n} | ${10n}       | ${50}                      | ${0} | ${50}  | ${55n}
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${100} | ${75n}
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${150} | ${88n}
+      ${100n} | ${0n}        | ${50}                      | ${0} | ${200} | ${94n}
+      ${100n} | ${200n}      | ${50}                      | ${0} | ${25}  | ${171n}
+      ${100n} | ${200n}      | ${50}                      | ${0} | ${50}  | ${150n}
+      ${100n} | ${200n}      | ${50}                      | ${0} | ${100} | ${125n}
+      ${1n}   | ${0n}        | ${Number.MAX_SAFE_INTEGER} | ${0} | ${1}   | ${0n}
+    `(
+      'equals $expected when: power = $power, initialValue = $initialValue, gapHalvingPeriod = $gapHalvingPeriod, t0 = $t0, t1 = $t1',
+      ({
+        power,
+        initialValue,
+        gapHalvingPeriod,
+        t0,
+        t1,
+        expected,
+      }: EnergyCalculationParams & {
+        expected: bigint;
+      }) => {
+        expect(calculateEnergyCap({ power, initialValue, gapHalvingPeriod, t0, t1 })).toEqual(expected);
+      },
+    );
+  });
+
+  describe('Effective energy', () => {
+    it('returns the minimum of linear energy and cap', () => {
+      expect(
+        calculateEffectiveEnergy({
+          power: 100n,
+          energyCap: 200n,
+          energy: 200n,
           gapHalvingPeriod: 50,
-          t0: 50,
-          t1: 20,
+          t0: 0,
+          t1: 50,
         }),
-      ).toThrowError('Negative power');
+      ).toEqual({ energyCap: 150n, linearEnergy: 225n, energy: 150n });
     });
   });
 });
