fef85b464ca582be83ee7ef5dcc0ee898e1d8a35
Vsevolods Mihailovs <cheelahim@gmail.com>
2021-11-12 16:43:44 +0200

    Update IQ protocol deployment information

--- packages/discovery/src/deployments.ts
+++ packages/discovery/src/deployments.ts
@@ -13,11 +13,20 @@ export const deployments: IQContractDeployments = {
   },
   // Binance Smart Chain Testnet
   'eip155:97': {
-    [IQContractName.Enterprise]: '0x03bdc1dDaF62095BD31A61A6F0d80ACe43D597Af',
-    [IQContractName.EnterpriseFactory]: '0x69C490B4967D12dBadF0d9D2050c7BD87012a474',
-    [IQContractName.PowerToken]: '0xDf7a764d604064D5609EfAf9a96cCC5828bCf0e9',
-    [IQContractName.RentalToken]: '0x02f616606CDBF4eD3a2D7E5301c91097a10d92c4',
-    [IQContractName.StakeToken]: '0x83110719bfB763D3c1d81cC97AbFbd18F6eA3Fe3',
+    [IQContractName.Enterprise]: '0x28186aDfe931cf3e529362bA10932C71dc5B0669',
+    [IQContractName.EnterpriseFactory]: '0xC98D0Ebb8904a0Aa15036379017770882aA14AE1',
+    [IQContractName.PowerToken]: '0xeA59D44f72A1Db24934b013424967e1e8750c7Bc',
+    [IQContractName.RentalToken]: '0x261a4F072929CFF97f45A8ED1340e092eC50E9Cc',
+    [IQContractName.StakeToken]: '0x4c38877C34E99B8847B950e474551dE2a9E72EF7',
     [IQContractName.DefaultConverter]: '0x59F089539CC0d7680f1b859c187b7161e4F9421d',
+  },
+  // Polygon Testnet Mumbai
+  'eip155:80001': {
+    [IQContractName.Enterprise]: '0x833DA99fa17b6d335DDEccC54626d6039a93277E',
+    [IQContractName.EnterpriseFactory]: '0xAe90F3c94Ec3649CF616061b66f28dbFEE747d3D',
+    [IQContractName.PowerToken]: '0xc7459C308c05eDeEf331B2eB3Dc44296129B775f',
+    [IQContractName.RentalToken]: '0xE6FA2C67c45A1D8DF0A57b632351f2C18Ea76F09',
+    [IQContractName.StakeToken]: '0x9aF78eddD01b3dc1D800A16AB04Db0E6FD6824a2',
+    [IQContractName.DefaultConverter]: '0x93Dc01F4C24698EaAF7777F391Cc75F8FcbbF5C5',
   },
 };
