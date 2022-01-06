fc2fd15988b09c4ff7fb53ca6896d68a5c588d61
Roberts Ivanovs <33699735+roberts-ivanovs@users.noreply.github.com>
2022-01-06 18:21:23 +0200

    Discovery info update (Polygon, BSC) (#3)
    * Added Polygon Mainnet chain and contract support
    * Updated Binance Smart Chain deployment addresses

--- packages/discovery/src/deployments.ts
+++ packages/discovery/src/deployments.ts
@@ -4,11 +4,11 @@ import { IQContractDeployments, IQContractName } from './types';
 export const deployments: IQContractDeployments = {
   // Binance Smart Chain Mainnet
   'eip155:56': {
-    [IQContractName.Enterprise]: '0xF5b264F028Ca5F075a7756A1C87dd5B36e14F4F6',
-    [IQContractName.EnterpriseFactory]: '0x8095c742dBd0e0a375D02c54f84A3AD3771BA932',
-    [IQContractName.PowerToken]: '0x9079bCbF55650b4760612a310DFA1458eC4CF0c5',
-    [IQContractName.RentalToken]: '0xE0582Cc604005457dd0Bf8D2bE82845DDdE8efea',
-    [IQContractName.StakeToken]: '0x7c6d55dc2787A8aA7cA7F929E0E99b465aAcA405',
+    [IQContractName.Enterprise]: '0x0Fa5914616c81245e0C306A99F0c01E26649D0dF',
+    [IQContractName.EnterpriseFactory]: '0x09B943cD0F7cA48A99A35a580CEf37E07B5c1dae',
+    [IQContractName.PowerToken]: '0xC4110b723a785d4F23E0547BB21c82E348112D89',
+    [IQContractName.RentalToken]: '0x55F4A8f015d14e55ff05935B391Af4a75f5dbBc1',
+    [IQContractName.StakeToken]: '0xAdB3e6c787e6D91DFcA4679EF6575a3745837E47',
     [IQContractName.DefaultConverter]: '0x7c88605dDB1f5eD8e7689088eBC77D82dee87f11',
   },
   // Binance Smart Chain Testnet
@@ -28,5 +28,14 @@ export const deployments: IQContractDeployments = {
     [IQContractName.RentalToken]: '0xE6FA2C67c45A1D8DF0A57b632351f2C18Ea76F09',
     [IQContractName.StakeToken]: '0x9aF78eddD01b3dc1D800A16AB04Db0E6FD6824a2',
     [IQContractName.DefaultConverter]: '0x93Dc01F4C24698EaAF7777F391Cc75F8FcbbF5C5',
+  },
+  // Polygon Mainnet
+  'eip155:137': {
+    [IQContractName.Enterprise]: '0x4ba78695845eb5e0a02cee3b51ffa214996032ce',
+    [IQContractName.EnterpriseFactory]: '0x908AC335219D13276D31Fa68449f7f63B1731BA6',
+    [IQContractName.PowerToken]: '0xcEeef2a276109DE31750519405fBaF66A5938A3E',
+    [IQContractName.RentalToken]: '0x7eB8D59514747e1d8A42519Ec08EbaB91cB8E1F2',
+    [IQContractName.StakeToken]: '0x27cdca6265B971D1aF8E46164F6E5494997dC449',
+    [IQContractName.DefaultConverter]: '0xf3e20Dc3F8df51bd608187dA09a649d081ee5B35',
   },
 };
