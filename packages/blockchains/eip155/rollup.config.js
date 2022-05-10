9f8651eae45b10bb87d27fd477c5878c59476cc6
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-05-10 17:26:48 +0300

    Remove redundant build step

--- packages/blockchains/eip155/rollup.config.js
+++ packages/blockchains/eip155/rollup.config.js
@@ -1,15 +1,3 @@
-import { buildConfig, buildPluginsSection } from '../../../rollup.config';
+import { buildConfig } from '../../../rollup.config';
 import pkg from './package.json';
-import copy from 'rollup-plugin-copy'
-
-const plugins = buildPluginsSection();
-plugins.push(
-  copy({
-    targets: [
-      // copy typechain declarations files
-      { src: 'src/contracts/*.d.ts', dest: 'dist' },
-    ]
-  })
-);
-
-export default buildConfig({ pkg, plugins });
+export default buildConfig({ pkg });
