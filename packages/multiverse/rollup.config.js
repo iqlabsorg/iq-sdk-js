2af908532b0cb871343410f073f595d34d6d7bb9
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-05-12 10:43:44 +0300

    Use typechain v8

--- packages/multiverse/rollup.config.js
+++ packages/multiverse/rollup.config.js
@@ -1,16 +1,3 @@
-import { buildConfig, buildPluginsSection } from '../../rollup.config';
+import { buildConfig } from '../../rollup.config';
 import pkg from './package.json';
-import copy from 'rollup-plugin-copy'
-
-const plugins = buildPluginsSection();
-plugins.push(
-  copy({
-    targets: [
-      // copy typechain declarations files
-      { src: 'src/contracts/*.d.ts', dest: 'dist/cjs' },
-      { src: 'src/contracts/*.d.ts', dest: 'dist/esm' },
-    ]
-  })
-);
-
-export default buildConfig({ pkg, plugins });
+export default buildConfig({ pkg });
